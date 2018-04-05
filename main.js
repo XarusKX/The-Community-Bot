5ece256cffff4b92c28300a3d9ab6ec642e5840e
Anthony Ricardo <anthony.ricardo@mikroskil.ac.id>
2018-04-05 10:59:24 +0700

    Load separate modules

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ const fs = require("fs");
 const Enmap = require("enmap");
 const EnmapLevel = require("enmap-level");
 Client.config = require("./config.js");
-Client.db1 = Database.Database;
-
-
+Client.db1 = Database.db1;
 
 Client.commands = new Enmap();
 Client.aliases = new Enmap();
@@ -15,9 +13,9 @@ Client.aliases = new Enmap();
 fs.readdir('./commands/', (err, files) => {
   if (err) console.error(err);
   console.log(`${Client.config.cliColor("GREEN")}Loading a total of ${files.length} commands.`);
-  files.forEach(f => {
-    if (f.split(".").slice(-1)[0] !== "js") return;
-    let props = require(`./commands/${f}`);
+  files.forEach(file => {
+    if (file.split(".").slice(-1)[0] !== "js") return;
+    let props = require(`./commands/${file}`);
     Client.commands.set(props.help.name, props);
     if(props.init) props.init(Client);
     props.conf.aliases.forEach(alias => {
@@ -30,10 +28,20 @@ fs.readdir('./events/', (err, files) => {
   if (err) console.error(err);
   console.log(`${Client.config.cliColor("GREEN")}Loading a total of ${files.length} events.`);
   files.forEach(file => {
+    if (file.split(".").slice(-1)[0] !== "js") return
     const eventName = file.split(".")[0];
     const event = require(`./events/${file}`);
     Client.on(eventName, event.bind(null, Client));
     delete require.cache[require.resolve(`./events/${file}`)];
+  });
+});
+
+fs.readdir('./modules/', (err, files) => {
+  if (err) console.error(err);
+  console.log(`${Client.config.cliColor("GREEN")}Loading a total of ${files.length} modules.`);
+  files.forEach(file => {
+    if (file.split(".").slice(-1)[0] !== "js") return;
+    let props = require(`./modules/${file}`);
   });
 });
 
