c478b17697220efbbb5731cda0e1072606e1e06c
Anthony Ricardo Thiotanry <belzeharkxarus@ymail.com>
2018-04-06 10:43:04 +0700

    Add new module

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,10 @@
 module.exports = async (client, message) => {
-  console.log(`${client.config.cliColor("NC")}I am ready!`);
   client.db1.Db.authenticate()
     .then(() => {
-      console.log(`${client.config.cliColor("GREEN")}Connection has been established!${client.config.cliColor("NC")}`);
+      console.log(`${client.config.cliColor("GREEN")}Database connection has been established!${client.config.cliColor("NC")}`);
     })
     .catch(err => {
-      console.error(`${client.config.cliColor("RED")}Unable to connect!${client.config.cliColor("NC")}`);
+      console.error(`${client.config.cliColor("RED")}Unable to connect to database!${client.config.cliColor("NC")}`);
       return;
     })
 }
---
+++ modules/array_functionality.js
@@ -0,0 +1,4 @@
+Array.prototype.joinByIndex = function (separator, start = 0, end = this.length - 1) {
+  end++;
+  return this.slice(start,end).join(separator);
+}
